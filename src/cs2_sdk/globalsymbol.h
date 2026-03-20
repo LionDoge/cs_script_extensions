f2656ba243bfc91aec977e7eb55a19e0b7d47c95
Lion Doge <KrystianoXPL@gmail.com>
2026-03-20 21:10:58 +0100

    Link MakeGlobalSymbol properly

--- src/cs2_sdk/globalsymbol.h
+++ src/cs2_sdk/globalsymbol.h
@@ -49,44 +49,10 @@ struct GlobalSymbolHashFunctor
 };
 
 namespace {
-	typedef const char* (*MakeGlobalSymbolFunc)(const char*);
-	inline static MakeGlobalSymbolFunc _makeGlobalSymbolFunc = nullptr;
+	PLATFORM_INTERFACE CGlobalSymbol _MakeGlobalSymbol(const char* str);
+	PLATFORM_INTERFACE CGlobalSymbolCaseSensitive _MakeGlobalSymbolCaseSensitive(const char* str);
+	PLATFORM_INTERFACE CGlobalSymbol _FindGlobalSymbol(const char* str);
+	PLATFORM_INTERFACE CGlobalSymbol _FindGlobalSymbolByHash(uint32 hash);
 }
 
-inline const char* MakeGlobalSymbol(const char* str)
-{
-	// TODO: Move out of here, fix the linking.
-#ifdef _WIN32
-	if (!_makeGlobalSymbolFunc)
-	{
-		HMODULE hModule = GetModuleHandleA("tier0.dll");
-		if (hModule == NULL) {
-			std::cerr << "Failed to get module handle. Error: " << GetLastError() << std::endl;
-			return nullptr;
-		}
-		FARPROC funcAddress = GetProcAddress(hModule, "_MakeGlobalSymbol");
-
-		_makeGlobalSymbolFunc = reinterpret_cast<MakeGlobalSymbolFunc>(funcAddress);
-	}
-	return _makeGlobalSymbolFunc(str);
-#else
-	if (!_makeGlobalSymbolFunc)
-	{
-		void* hModule = dlopen("libtier0.so", RTLD_LAZY);
-		if (hModule == NULL) {
-			std::cerr << "Failed to load library. Error: " << dlerror() << std::endl;
-		}
-		void* funcAddress = dlsym(hModule, "_MakeGlobalSymbol");
-		if (funcAddress == NULL) {
-			std::cerr << "Failed to find symbol. Error: " << dlerror() << std::endl;
-		}
-
-		_makeGlobalSymbolFunc = reinterpret_cast<MakeGlobalSymbolFunc>(funcAddress);
-	}
-	return _makeGlobalSymbolFunc(str);
-#endif
-}
-
-PLATFORM_INTERFACE CGlobalSymbolCaseSensitive MakeGlobalSymbolCaseSensitive(const char* str);
-PLATFORM_INTERFACE CGlobalSymbol FindGlobalSymbol(const char* str);
-PLATFORM_INTERFACE CGlobalSymbol FindGlobalSymbolByHash(uint32 hash);
+#define MakeGlobalSymbol(str) _MakeGlobalSymbol(str)
