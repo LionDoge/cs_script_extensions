0c6050ea2e1b709cfa12fcce7de19e1b41efd0c6
Lion Doge <KrystianoXPL@gmail.com>
2026-04-19 23:39:42 +0200

    Update copyright notices with applied changes

--- src/cs2_sdk/schema.h
+++ src/cs2_sdk/schema.h
@@ -15,6 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Modified on 19.04.2026 by liondoge - track reflection info for script access
  */
 
 #pragma once
